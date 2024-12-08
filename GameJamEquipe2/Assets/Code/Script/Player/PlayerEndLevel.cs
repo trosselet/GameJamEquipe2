41fbcb1ceef7af427dfa2222b518b7efbc0c57c4
Thomas <trosselet@gaming.tech>
2024-12-08 11:29:01 +0100

    [A]

--- GameJamEquipe2/Assets/Code/Script/Player/PlayerEndLevel.cs
+++ GameJamEquipe2/Assets/Code/Script/Player/PlayerEndLevel.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerEndLevel : MonoBehaviour
 {
@@ -10,28 +7,27 @@ public class PlayerEndLevel : MonoBehaviour
     [SerializeField] private bool timerInProgress = false;
     [SerializeField] private TextMeshProUGUI timerText;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         timerInProgress = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (timerInProgress)
         {
             timer += Time.deltaTime;
-            timerText.text = timer.ToString();
+
+            timerText.text = timer.ToString("F2");
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Finish")
+        if (other.CompareTag("Finish"))
         {
             timerInProgress = false;
+
             Destroy(other.gameObject);
         }
     }
